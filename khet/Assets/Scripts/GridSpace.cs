6fcc68107c29895bdbbb3f83597c91ba50626151
CormacRaftery <cormacinho@hotmail.com>
2020-08-02 22:12:30 +0000

    attempt at khet

---
+++ khet/Assets/Scripts/GridSpace.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.UI;
+public class GridSpace : MonoBehaviour
+{
+    /*   // Start is called before the first frame update
+       void Start()
+       {
+
+       }
+
+       // Update is called once per frame
+       void Update()
+       {
+
+       }*/
+
+    public Button button;
+    public Text buttonText;
+    public string playerSide;
+    public int horizontalRow;
+    public int verticalRow;
+
+    public void SetSpace()
+    {
+        //buttonText.text = playerSide;
+        //button.interactable = false;
+       // EventLog("piece selected" + button);
+        print("piece selected" + button);
+
+        if (buttonText.text == "Pyramid")
+        {
+            buttonText.text = playerSide;
+            button.interactable = false;
+        }
+        
+    }
+}
---
+++ khet/Assets/Scripts/PieceBehaviour.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PieceBehaviour : MonoBehaviour
+{
+}
+    /*   // Start is called before the first frame update
+       void Start()
+       {
+
+       }
+
+       // Update is called once per frame
+       void Update()
+       {
+
+       }*/
+ /*   public Boolean PieceSelected = false;
+    public Button button;
+    public Button antiClockwiseButton;
+    public Button clockwiseButton;
+    public Text buttonText;
+    public string playerSide;
+
+    public void PickedPiece()
+    {
+        PieceSelected = true;
+        if (antiClockwiseButton.pressed() && PieceSelected)
+        {
+            button.rotateImage("left");
+        }
+        if (clockwiseButton.pressed() && PieceSelected)
+        {
+            button.rotateImage("right");
+        }
+    }
+
+    public void SetSpace()
+    {
+        buttonText.text = playerSide;
+        button.interactable = false;
+    }
+}
+ */
