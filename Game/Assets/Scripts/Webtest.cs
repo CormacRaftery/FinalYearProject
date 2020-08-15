4149a19113fbac0d7ce9c3464ba7fa08de330e1e
CormacRaftery <cormacinho@hotmail.com>
2020-08-15 20:00:30 +0000

    Tic Tac Toe and sql db

---
+++ Game/Assets/Scripts/Webtest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.Cache;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+using Debug = UnityEngine.Debug;
+
+public class Webtest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        WWW request = new WWW("Http://localhost/sqlconnect/webtest.php");
+        yield return request;
+       string[] webResults = request.text.Split('\t');
+
+        foreach(string s in webResults)
+        {
+
+        }
+    }
+
+}
